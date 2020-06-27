47b0a7e0727c21427ce88dadb04930c3c9f36956
ronbarrantes <ronbarrantes@gmail.com>
2020-06-27 02:00:02 -0700

    Create the board state

--- src/BingoCard.ts
+++ src/BingoCard.ts
@@ -2,44 +2,45 @@ import * as utils from './utils'
 
 class BingoCard {
 
-  private getOneCol(from: number, to: number): number[]{
+  /**
+   * get a sequence of number and returns 5 random numbers from said sequence
+   * @param from start of the range 
+   * @param to end of the range
+   * @returns 5 random numbers from the range
+   */
+  private get5RandomNumbersFromRange(from: number, to: number): number[]{
     let orderedList = []
     let resultList = []
+
+    if(from + 4 > to){
+      throw new Error('from value needs to be at least 5 numbers bigger');
+    }
 
     for(let i = from; i <= to; i++){
       orderedList.push(i)
     }
 
-    // ERROR: there getting multiple of the same number
-    for(let i = 0; i < 5; i++){
-      const num = utils.randomNumber(orderedList.length)
-      resultList.push(orderedList[num])
-      const trimList: number[] = [...orderedList.slice(0, num -1), ...orderedList.slice(num, orderedList.length)]
-      orderedList = trimList
-    }
-
+    resultList = utils.shuffleArray(orderedList).splice(0, 5)
     return resultList
   }
   
   private createState() {
-    let board = new Array(5).fill([])
-
-    let numberList: number[] = []
-
+    let board: number[][] = []
+    let numberList: number[][] = []
     for(let cols = 0; cols < 5; cols++){
       const inc = 15 * cols
-      const from = 1 + inc
-      const to = from + 14
-      numberList = [ ...numberList, ...this.getOneCol(from, to) ]
+      const from = 1 + inc 
+      const to = from + 14 
+      numberList.push(this.get5RandomNumbersFromRange(from, to))
     }
-
-    for(let cols = 0; cols < 5; cols++){
-      for(let rows = 0; rows < 5; rows++){
-        board[rows][cols] = utils.randomNumber(100)
+    
+    for (let i = 0; i < numberList.length; i++) {
+      board.push([])
+      for (let j = 0; j < numberList[i].length; j++) {
+        board[i].push(numberList[j][i])
       }
     }
 
-    // TODO: add the numbers to the board array as a string
     return board
   }
 
--- src/utils.ts
+++ src/utils.ts
@@ -30,3 +30,27 @@ export const randomColor = (): string =>
  */
 export const appendMultiple: AppendMultiple = (parent) =>
   (...children) => children.forEach(child => parent.appendChild(child))
+
+/**
+ * Shuffles and array
+ * @param array array to be shuffled
+ * @returns shuffled array
+ */
+export const shuffleArray = (array: number[]): number[] => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
